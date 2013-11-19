c0945c96de0c08e423cecc6d34410ac53885fb21
Eileen <eileen@fuzion.co.nz>
2013-11-19 15:27:02 +1300

    Fix Class capitalisation

--- CRM/Csvimport/Import/Form/Preview.php
+++ CRM/Csvimport/Import/Form/Preview.php
@@ -1,5 +1,5 @@
 <?php
-class CRM_Csvimport_Import_Form_Preview extends CRM_Csvimport_Import_Form_Previewbaseclass {
+class CRM_Csvimport_Import_Form_Preview extends CRM_Csvimport_Import_Form_PreviewBaseClass {
   public $_parser = 'CRM_Csvimport_Import_Parser_Api';
 
 }
