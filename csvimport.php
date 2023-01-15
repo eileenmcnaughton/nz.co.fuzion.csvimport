7bc34cfa242166c4153ee18da06ea9c1bd41d019
Eileen McNaughton <emcnaughton@wikimedia.org>
2023-01-16 09:14:14 +1300

    civix upgrade

--- csvimport.php
+++ csvimport.php
@@ -19,37 +19,10 @@ function csvimport_civicrm_install() {
 }
 
 /**
- * Implementation of hook_civicrm_uninstall
- */
-function csvimport_civicrm_uninstall() {
-  _csvimport_civix_civicrm_uninstall();
-}
-
-/**
  * Implementation of hook_civicrm_enable
  */
 function csvimport_civicrm_enable() {
   _csvimport_civix_civicrm_enable();
-}
-
-/**
- * Implementation of hook_civicrm_disable
- */
-function csvimport_civicrm_disable() {
-  _csvimport_civix_civicrm_disable();
-}
-
-/**
- * Implementation of hook_civicrm_upgrade
- *
- * @param $op string, the type of operation being performed; 'check' or 'enqueue'
- * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
- *
- * @return mixed  based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
- *                for 'enqueue', returns void
- */
-function csvimport_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _csvimport_civix_civicrm_upgrade($op, $queue);
 }
 
 /**
@@ -70,13 +43,4 @@ function csvimport_civicrm_navigationMenu(&$menu) {
   ];
   _csvimport_civix_insert_navigation_menu($menu, 'Administer', $item[0]);
   _csvimport_civix_navigationMenu($menu);
-}
-
-/**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function csvimport_civicrm_postInstall() {
-  _csvimport_civix_civicrm_postInstall();
 }
